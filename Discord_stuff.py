149a331259202f2c26a2f403abc16ee9428573bd
kriskros341 <krzysztofczuba884@gmail.com>
2021-01-27 14:58:42 +0100

    some much needed refactoring for DiscordWsHandler

--- Discord_stuff.py
+++ Discord_stuff.py
@@ -3,7 +3,7 @@ from tornado.websocket import websocket_connect
 import json
 import asyncio
 import datetime
-from cert import bot_token
+from cert import *
 
 my_loop = asyncio.get_event_loop()
 
@@ -32,12 +32,13 @@ class UserObject:
         return {"id": self.id, "online": self.voice_state, "saved_datetime": self.saved_time, "img_id": self.avatar}
 
 
-def create_discord_http(auth_token, db):
-    return DiscordHttp(auth_token, db)
+def create_discord_http(db):
+    return DiscordHttp(db)
 
 
 class DiscordHttp:
     def __init__(self, db):
+        self.db = db
         db.set_discord_http_interface(self)
         self.bot_headers = {
                             "User-Agent": "DiscordBot($url, $versionNumber)",
@@ -47,6 +48,17 @@ class DiscordHttp:
     def get_user_data(self, user_id):
         requested_data = requests.get(f"https://discord.com/api/v8/users/{user_id}", headers=self.bot_headers)
         return requested_data.json()
+
+    def update_guilds(self):
+        guilds = requests.get("https://discord.com/api/v8/users/@me/guilds", headers=self.bot_headers)
+        recorded_guilds = self.db.get_data_guilds()
+        print(guilds.json())
+        print(recorded_guilds)
+        #u = self.db.find_which_guild_user_is_in
+        for guild_data in guilds.json():
+            if guild_data['id'] not in [g['id'] for g in recorded_guilds]:
+                self.db.create_guild(guild_data)
+
 
 
 def create_discord_ws(function=None, database=None):
@@ -62,24 +74,25 @@ class DiscordWsHandler:
         self.functional_interface = None
         asyncio.ensure_future(self.get_con(), loop=my_loop)
 
-    def set_functional_interface(self, functional_interface, db):
+    def set_functional_interface(self, functional_interface: str, db) -> None:
         assert functional_interface is not None, \
-            print(f"{functional_interface} is not valid a valid functional interface")
+            print(f"{functional_interface} is not a valid functional interface")
         self.functional_interface = interface_factory(functional_interface, db)
 
-    async def get_con(self):
+    async def get_con(self) -> None:
         ws = await websocket_connect("wss://gateway.discord.gg/?v=6&encoding=json")
         self.connection = ws
         asyncio.ensure_future(self.send_handshake_and_start_pinging(ws), loop=my_loop)
         await self.await_messages()
 
-    async def send_handshake_and_start_pinging(self, ws):
+    async def send_handshake_and_start_pinging(self, ws) -> None:
+        """ uzycie self.connection == ws mi sie nie podoba. """
         ws.write_message(json.dumps(DISCORD_HELLO_OBJECT))
         while self.connection == ws:
             self.connection.write_message(json.dumps({"op": 1, "d": self.sequence_number}))
             await asyncio.sleep(41.25)
 
-    async def await_messages(self):
+    async def await_messages(self) -> None:
         if self.connection:
             assert self.functional_interface, print("Feature not implemented")
             while msg := await self.connection.read_message():
@@ -101,6 +114,24 @@ class VoiceStateObserver:
     def interpret_data(self, msg):
         if msg['t'] == "VOICE_STATE_UPDATE":
             usr = UserObject(msg['d'])
+            if msg['d']['channel_id'] is None:
+                self.db.user_set_online_state(usr.get_data(), False)
+            else:
+                db_data = [x for x in self.db.fetch_all()]
+                if int(msg['d']['user_id']) in [int(u[1]) for u in db_data]:
+                    self.db.user_set_online_state(usr.get_data(), True)
+                    pass
+                else:
+                    self.db.create_user(usr.get_data(), msg['d']['guild_id'])
+
+class GuildStateObserver:
+    def __init__(self, db):
+        self.db = db
+
+    def interpret_data(self, msg):
+        if msg['t'] == "GUILD_MEMBER_UPDATE":
+            if msg['d']['user']['id'] == bot_id:
+                usr = UserObject(msg['d'])
             if msg['d']['channel_id'] is None:
                 self.db.user_set_online_state(usr.get_data(), False)
             else:
--- main.py
+++ main.py
@@ -9,8 +9,6 @@ from Discord_stuff import *
 PORT = 2137
 
 
-active_connections = []
-
 def test1(con_list, data):
     print(f"data to {con_list}")
     for controller in con_list:
@@ -26,16 +24,22 @@ class HTTPController(tornado.web.RequestHandler):
         return True
 
     def get(self):
+
         active_users = self.db.fetch_active_users_from_db()
-        self.write(json.dumps({"active":active_users}))
+        self.write(json.dumps({"active": active_users}))
+
+
+
+
+
+
+
 
 
 class Controller(WebSocketHandler):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.db = db
-        self.con_list = active_connections
-        active_connections.append(self)
         self.discord_ws = discord_ws
         self.discord_ws.connections.append(self)
         self.discord_http = discord_http
@@ -52,7 +56,7 @@ class Controller(WebSocketHandler):
         msg = json.loads(msg)
         if msg['option'] == "fetch_all":
             data = db.fetch_users_active_from_db()
-            data = json.dumps({"msg":"user_data", "id":data[0], "username":data[1]})
+            data = json.dumps({"msg": "user_data", "id":data[0], "username":data[1]})
             self.write_message(data)
         if msg['option'] == "state_of_discord":
             data = self.discord_http.get_user_data(267460142889566209)
@@ -73,6 +77,9 @@ if __name__ == "__main__":
     db = DB_handler()
     print(f"Listening localhost on {PORT}")
     discord_http = create_discord_http(db)
+
+    discord_http.update_guilds()
+
     discord_ws = create_discord_ws(function="VOICE_STATE_OBSERVER", database=db)
     asyncio.ensure_future(discord_ws.await_messages(), loop=my_loop)
 
